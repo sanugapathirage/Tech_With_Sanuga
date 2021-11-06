5992fada290868066b512d544cd1933a31d3fc5c
Sanuga_Pathirage <sanugadulsara@gmail.com>
2021-11-06 11:45:03 +0530

    Code_Video_6_A
    This is the code from my 6th video for testing an Ultrasonic Sensor

---
+++ Code_Video_6_A/Code_Video_6_A/Code_Video_6_A.ino
@@ -0,0 +1,21 @@
+#include <NewPing.h>
+#define TRIGGER_PIN  9
+#define ECHO_PIN     10
+#define MAX_DISTANCE 400 //This is the max distance for the HC-SR04 Ultrasonic Sensor
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(9600);
+}
+ 
+void loop() {
+  Serial.print("Distance is: ");
+  Serial.println(sonar.ping_cm());
+  delay(1000);
+}
+
+
+
+
+
+
